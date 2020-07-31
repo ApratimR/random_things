7a83c49a1cfa9329cc07710983dccf39b6d7745e
ApratimR <apratimr55@gmail.com>
2020-07-31 16:15:27 +0530

    🧹 comments clean-up on map reduce script

--- mapred.py
+++ mapred.py
@@ -1,22 +1,21 @@
 string1 = str(input("enter a string"))
 
-print(string1)
+#print(string1)
 
-#spliting data
+#spliting data with imaginary value of 1
 string1 = string1.split()
 
-
+#sorting
 string1.sort()
 
-
+#empty info dictionary
 reduceddata = {} 
 
+#reduce
 for temp in string1:
 	if temp in reduceddata.keys():
-		#reduction
 		reduceddata[temp]+=1
 	else:
-		#maps
 		reduceddata.update({temp : 1})
 
 print(reduceddata)
